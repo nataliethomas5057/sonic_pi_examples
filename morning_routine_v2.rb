81b311d36a704ccfd179ba091398a501d91ee0db
Natalie Thomas <Natalie Thomas>
2016-08-22 08:16:45 -0400

    morning routine version 2

---
+++ morning_routine_v2.rb
@@ -0,0 +1,23 @@
+def door
+  play(70)
+  sleep(1)
+  play(40)
+  sleep(1)
+end
+def feed(animals)
+  sample(animals)
+  sleep(2)
+end
+def coffee(make_and_drink)
+  sample(make_and_drink)
+  sleep(2)
+end
+door
+feed(:bass_hard_c)
+door
+feed(:guit_e_slide)
+door
+coffee(:bass_dnb_f)
+door
+sleep(2)
+coffee(:ambi_choir)
